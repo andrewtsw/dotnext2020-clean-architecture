d8d37787d59a7787f5181240bd946bed18e96819
Andrei Tsvettsikh <a.tsvettsikh@pravo.tech>
2020-10-19 19:15:09 +0700

    8. Move controllers to separate project

--- Dotnext.CleanArchitecture/Dotnext.Controllers/OrdersController.cs
+++ Dotnext.CleanArchitecture/Dotnext.Controllers/OrdersController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Dotnext.Web.Controllers
+namespace Dotnext.Controllers
 {
     [ApiController]
     [Route("[controller]")]
